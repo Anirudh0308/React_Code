f9df968cd57dd72b37fcdc5d49dcf5e6829ca612
Anirudh0308 <129490209+Anirudh0308@users.noreply.github.com>
2024-08-04 20:33:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+import Profile from './profile';
+function App() {
+  
+  
+    return (
+      <div className="App">
+      <h1>Conditional Rendering</h1>
+      <Profile/>
+      </div>
+    );
+}
+
+export default App;
